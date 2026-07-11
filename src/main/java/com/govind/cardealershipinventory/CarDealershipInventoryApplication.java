b1023b593456df2f5f8f824a9b0973c0c5e4996f
govin <govindbhochiya038@gmail.com>
2026-07-11 17:52:59 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/govind/cardealershipinventory/CarDealershipInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.govind.cardealershipinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipInventoryApplication.class, args);
+	}
+
+}
