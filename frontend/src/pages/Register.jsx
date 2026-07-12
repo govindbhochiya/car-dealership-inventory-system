44201fc5d8e7a9fcbaf0e6c1f571239bbda282b2
govin <govindbhochiya038@gmail.com>
2026-07-12 15:59:08 +0530

    feat: implement user registration page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,70 @@
+import { Link } from "react-router-dom";
+
 function Register() {
-    return <h1>Register Page</h1>;
+
+    return (
+        <div>
+
+            <h1>User Registration</h1>
+
+            <form>
+
+                <div>
+                    <label>Full Name</label><br />
+                    <input
+                        type="text"
+                        placeholder="Enter Full Name"
+                    />
+                </div>
+
+                <br />
+
+                <div>
+                    <label>Email</label><br />
+                    <input
+                        type="email"
+                        placeholder="Enter Email"
+                    />
+                </div>
+
+                <br />
+
+                <div>
+                    <label>Password</label><br />
+                    <input
+                        type="password"
+                        placeholder="Enter Password"
+                    />
+                </div>
+
+                <br />
+
+                <div>
+                    <label>Role</label><br />
+                    <select>
+                        <option value="">Select Role</option>
+                        <option value="USER">USER</option>
+                        <option value="ADMIN">ADMIN</option>
+                    </select>
+                </div>
+
+                <br />
+
+                <button type="submit">
+                    Register
+                </button>
+
+            </form>
+
+            <br />
+
+            <p>
+                Already have an account?
+                <Link to="/login"> Login</Link>
+            </p>
+
+        </div>
+    );
 }
 
 export default Register;
